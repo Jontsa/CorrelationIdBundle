75577fce357f1c93cd9449d38cd9603d2d0edfbe
Jontsa <joni.halme@sirumobile.com>
2020-11-23 17:20:22 +0200

    add composer.json and bundle file

---
+++ src/JontsaCorrelationIdBundle.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Jontsa\Bundle\CorrelationIdBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JontsaCorrelationIdBundle extends Bundle
+{
+
+}
